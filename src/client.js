7addf0821e23ecc742e36255aa869bd20b4cddb0
Sara Carlstein <carlstein.sara@gmail.com>
2021-08-17 13:41:56 +0200

    added widgets to trigger deploy

--- src/client.js
+++ src/client.js
@@ -4,5 +4,7 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "ha8pf34i", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
+  implements: "part:@sanity/dashboard/config",
+  path: "src/dashboardConfig.js",
   useCdn: true,
 });
---
+++ src/dashboardConfig.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Sanity Studio",
+            apiId: "b78ab9c3-1a5c-4306-8a5b-b8d19ca0267a",
+            buildHookId: "611b9f8429a61012dd6de9e2",
+            name: "distracted-pasteur-892e75",
+          },
+        ],
+      },
+    },
+  ],
+};
